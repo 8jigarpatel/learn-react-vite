7f4bdc2c3e56e144727669963455c3c4f033a98a
Jigar Patel <8.jigar.patel@gmail.com>
2023-05-12 12:37:42 -0600

    fix: configure eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
 function App() {
+  // eslint-disable-next-line no-console
   console.log(`Running in '${import.meta.env.MODE}' mode`);
   const [count, setCount] = React.useState(0);
   return (
     <>
-      <h1 className='text-5xl font-bold py-5 text-center bg-green-500'>
+      <h1 className="text-5xl font-bold py-5 text-center bg-green-500">
         Vite + React + Tailwind
       </h1>
-      <h1 className='text-7xl py-10 text-center my-auto'>
-        Count: {count}
-      </h1>
-      <div className='text-center'>
-        <button className='btn bg-red-500 mx-2 text-white' onClick={() => setCount((count) => count - 1)}>
+      <h1 className="text-7xl py-10 text-center my-auto">Count: {count}</h1>
+      <div className="text-center">
+        <button
+          type="button"
+          className="btn bg-red-500 mx-2 text-white"
+          onClick={() => setCount(count - 1)}
+        >
           -
         </button>
-        <button className='btn bg-blue-500 mx-2 text-white' onClick={() => setCount(0)}>
+        <button
+          type="button"
+          className="btn bg-blue-500 mx-2 text-white"
+          onClick={() => setCount(0)}
+        >
           0
         </button>
-        <button className='btn bg-green-500 mx-2 text-white' onClick={() => setCount((count) => count + 1)}>
+        <button
+          type="button"
+          className="btn bg-green-500 mx-2 text-white"
+          onClick={() => setCount(count + 1)}
+        >
           +
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
