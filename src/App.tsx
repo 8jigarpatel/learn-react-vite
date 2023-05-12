59e2da38604d66595a83dc54095d2a302d8cdf25
AD_T580 <8.jigar.patel@gmail.com>
2023-05-11 18:27:05 -0600

    fix: misc changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import React from 'react';
 import './App.css'
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <>
-      <h1 className="text-5xl font-bold py-5 text-center bg-green-500">
+      <h1 className='text-5xl font-bold py-5 text-center bg-green-500'>
         Vite + React
       </h1>
+      <h1 className='text-7xl py-10 text-center my-auto'>
+        Count: {count}
+      </h1>
+      <div className='text-center'>
+        <button className='btn bg-green-500 mx-2 text-white' onClick={() => setCount((count) => count + 1)}>
+          +
+        </button>
+        <button className='btn bg-red-500 mx-2 text-white' onClick={() => setCount((count) => count - 1)}>
+          -
+        </button>
+      </div>
     </>
   )
 }
