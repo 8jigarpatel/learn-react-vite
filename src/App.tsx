e6ee12d9259f6117dbac50018b3717ab2e86a213
AD_T580 <8.jigar.patel@gmail.com>
2023-05-11 20:17:02 -0600

    fix: log app mode in console

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css'
 
 function App() {
+  console.log(`Running in '${import.meta.env.MODE}' mode`);
   const [count, setCount] = React.useState(0);
   return (
     <>
       <h1 className='text-5xl font-bold py-5 text-center bg-green-500'>
-        Vite + React
+        Vite + React + Tailwind
       </h1>
       <h1 className='text-7xl py-10 text-center my-auto'>
         Count: {count}
@@ -18,9 +19,6 @@ function App() {
         <button className='btn bg-red-500 mx-2 text-white' onClick={() => setCount((count) => count - 1)}>
           -
         </button>
-      </div>
-      <div>
-        <p className='fixed bottom-0'>[{import.meta.env.MODE}]</p>
       </div>
     </>
   )
