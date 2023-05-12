03b149947d48f5c163cee4b1efdcac4ae8cd21ca
Jigar Patel <8.jigar.patel@gmail.com>
2023-05-12 14:52:38 -0600

    fix: use HashRouter

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,19 @@
 function Home() {
   return (
-    <div className="text-center text-6xl py-16 font-mono">Hello, world!</div>
+    <>
+      <div className="text-center text-6xl py-16 font-mono">Hello, world!</div>
+      <div className="text-center">
+        Find this project on{' '}
+        <a
+          className="href"
+          href="https://github.com/8jigarpatel/learn-react-vite"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
+    </>
   );
 }
 
