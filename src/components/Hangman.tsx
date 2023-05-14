9c5291a8f2c4c55d01bf2f37520d257e09db3cde
Jigar Patel <8.jigar.patel@gmail.com>
2023-05-14 13:43:00 -0600

    chore: misc clean up

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -4,6 +4,7 @@ function Hangman() {
   // guess and secret
   const [guess, setGuess] = React.useState('');
   const [secret, setSecret] = React.useState('');
+
   const [playing, setPlaying] = React.useState(false);
   const [attemptLeft, setAttemptsLeft] = React.useState(0);
   const [win, setWin] = React.useState(false);
@@ -11,60 +12,56 @@ function Hangman() {
   const [tried, setTried] = React.useState('');
 
   useEffect(() => {
-    // win
-    if (secret !== '' && secret.toLowerCase() === guess.toLowerCase()) {
-      setWin(true);
+    if (!playing || secret === '') {
+      return;
     }
-    // game over
-    else if (
-      attemptLeft === 0 && playing &&
-      secret.toLowerCase() !== guess.toLowerCase()
-    ) {
+
+    if (secret === guess) {
+      setWin(true);
+    } else if (attemptLeft === 0 && secret !== guess) {
       setOver(true);
     }
   }, [attemptLeft, guess]);
 
-  useEffect(() => {
-    setGuess(secret.replace(/./g, '-'));
-  }, [secret]);
+  const updateGuess = (l: string) => {
+    let temp = guess;
+    for (let i = 0; i < temp.length; i += 1) {
+      if (secret[i] === l) {
+        temp = temp.slice(0, i) + secret[i] + temp.slice(i + 1);
+      }
+    }
+    setGuess(temp);
+  };
 
   const onInputClick = (l: string) => {
     if (playing) {
       setTried(tried + l);
       if (secret.includes(l)) {
-        let temp = guess;
-        for (let i = 0; i < temp.length; i++) {
-          if (secret[i] === l) {
-            temp = temp.slice(0, i) + secret[i] + temp.slice(i + 1);
-          }
-        }
-        setGuess(temp);
-      }
-      else {
+        updateGuess(l);
+      } else {
         setAttemptsLeft(attemptLeft - 1);
       }
     } else {
-      setSecret(secret + l);
+      setSecret((s) => {
+        return s + l;
+      });
     }
   };
 
   const onRestartClick = () => {
-    setGuess('');
-    setSecret('');
-    setTried('');
     setPlaying(false);
+    setSecret('');
+    setGuess('');
+    setAttemptsLeft(0);
+    setTried('');
     setWin(false);
     setOver(false);
-    setAttemptsLeft(0);
   };
 
   const onSetSecretClick = () => {
-    setAttemptsLeft(secret.length);
     setPlaying(true);
-  };
-
-  const isInputDisabled = (input: string): boolean => {
-    return tried.includes(input);
+    setAttemptsLeft(() => secret.length);
+    setGuess(secret.replace(/./g, '-'));
   };
 
   const getLetters = [
@@ -101,7 +98,7 @@ function Hangman() {
         type="button"
         tabIndex={-1}
         className="btn font-mono m-2 bg-blue-100 hover:bg-blue-300 disabled:bg-blue-800"
-        disabled={isInputDisabled(l)}
+        disabled={tried.includes(l)}
         onClick={() => onInputClick(l)}
       >
         {l}
@@ -138,47 +135,22 @@ function Hangman() {
             </div>
           )}
         </div>
-        {/* <div>Attempts left: {attemptLeft}</div> */}
         <div className="pt-2">
-          <ul className="grid grid-cols-4 lg:grid-cols-6 gap-2">
-            {getLetters}
-          </ul>
+          <ul className="grid grid-cols-6">{getLetters}</ul>
         </div>
         {win && (
-          <div className="my-5 py-2 rounded-lg text-white text-center bg-green-600">
-            You Won!
-            <button
-              type="button"
-              className="btn underline"
-              onClick={onRestartClick}
-            >
-              Restart
-            </button>
+          <div className="grid grid-flow-row gap-4 px-2">
+            <div className="my-5 py-2 rounded-lg text-white text-center bg-green-600">
+              You Won!
+            </div>
           </div>
         )}
         {over && (
           <div className="my-5 py-2 rounded-lg text-white text-center bg-red-600">
             You Lost!
-            <button
-              type="button"
-              className="btn underline"
-              onClick={onRestartClick}
-            >
-              Restart
-            </button>
           </div>
         )}
         <div className="text-gray-300 m-4">{attemptLeft}</div>
-        {false && (
-          <>
-            <div>playing: {playing}</div>
-            <div>secret: {secret}</div>
-            <div>guess: {guess}</div>
-            <div>{attemptLeft}</div>
-            <div>win: {win}</div>
-            <div>over: {over}</div>
-          </>
-        )}
       </div>
     </div>
   );
